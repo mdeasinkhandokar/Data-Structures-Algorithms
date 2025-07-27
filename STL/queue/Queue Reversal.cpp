941f379208b65be7c1ce8ea455b69aca983a2f37
mdeasinkhandokar <easinkhandokar2000@gmail.com>
2025-07-27 22:35:40 +0600

    queue

---
+++ STL/queue/Queue Reversal.cpp
@@ -0,0 +1,22 @@
+
+// function Template for C++
+///https://www.geeksforgeeks.org/problems/queue-reversal/1
+class Solution {
+  public:
+    queue<int> reverseQueue(queue<int> &q) {
+        // code here.
+
+        stack<int>st;
+
+        while(!q.empty()){
+            st.push(q.front());
+           q.pop();
+        }
+        while(!st.empty()){
+            q.push(st.top());
+            st.pop();
+        }
+        return q;
+
+    }
+};
